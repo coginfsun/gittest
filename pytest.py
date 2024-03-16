23fd0e9db6ba077d91331619d0f8b1cb51a567ff
Zitang Sun <zitangsun96@gmail.com>
2024-03-16 22:35:01 +0900

    message = "changes to be committed"

--- pytest.py
+++ pytest.py
@@ -1,35 +1,3 @@
-import tensorflow as tf
-from tensorflow.keras import layers
-import torch
-import torchvision
-
-# Load the MNIST dataset
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# Preprocess the data
-x_train = x_train / 255.0
-x_test = x_test / 255.0
-
-# Define the model architecture
-model = tf.keras.Sequential([
-    layers.Flatten(input_shape=(28, 28)),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(10, activation='softmax')
-])
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-# Train the model
-model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
-
-# Evaluate the model
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print(f'Test loss: {test_loss}')
-print(f'Test accuracy: {test_acc}')
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
